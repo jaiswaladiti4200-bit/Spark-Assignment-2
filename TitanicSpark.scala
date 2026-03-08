e352ed05f64a416cdd5837c5bd118486b46344ea
Aditi Jaiswal <jaiswal.adit@northeastern.edu>
2026-03-07 23:43:42 -0500

    Completed Titanic Spark Assignment 2 with ML prediction and accuracy evaluation

--- TitanicSpark.scala
+++ TitanicSpark.scala
@@ -1,5 +1,8 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 object TitanicSpark {
 
@@ -10,35 +13,131 @@ object TitanicSpark {
       .master("local[*]")
       .getOrCreate()
 
-    val df = spark.read
-      .option("header","true")
-      .option("inferSchema","true")
+    import spark.implicits._
+
+    // -------------------------------
+    // Load Training Data
+    // -------------------------------
+
+    val train = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
       .csv("train.csv")
 
-    df.show()
-    df.printSchema()
+    train.show()
+    train.printSchema()
 
-    // Missing values
-    df.select(
+    // -------------------------------
+    // Missing Values Analysis
+    // -------------------------------
+
+    train.select(
       count(when(col("Age").isNull, true)).alias("Missing Age"),
       count(when(col("Cabin").isNull, true)).alias("Missing Cabin"),
       count(when(col("Embarked").isNull, true)).alias("Missing Embarked")
     ).show()
 
-    // Survival count
-    df.groupBy("Survived").count().show()
+    // -------------------------------
+    // Survival Statistics
+    // -------------------------------
 
-    // Survival by gender
-    df.groupBy("Sex","Survived").count().show()
+    train.groupBy("Survived").count().show()
+    train.groupBy("Sex", "Survived").count().show()
+    train.groupBy("Pclass", "Survived").count().show()
 
-    // Survival by class
-    df.groupBy("Pclass","Survived").count().show()
+    train.select(avg("Age")).show()
+    train.select(avg("Fare")).show()
 
-    // Average age
-    df.select(avg("Age")).show()
+    // -------------------------------
+    // Handle Missing Values
+    // -------------------------------
 
-    // Average fare
-    df.select(avg("Fare")).show()
+    val avgAge = train.select(avg("Age")).first().getDouble(0)
+    val avgFare = train.select(avg("Fare")).first().getDouble(0)
+
+    val trainClean = train.na.fill(Map(
+      "Age" -> avgAge,
+      "Fare" -> avgFare
+    ))
+
+    // -------------------------------
+    // Feature Engineering
+    // -------------------------------
+
+    val trainFeatures = trainClean
+      .withColumn("SexNumeric", when(col("Sex") === "male", 1).otherwise(0))
+      .withColumn("FamilySize", col("SibSp") + col("Parch"))
+
+    // -------------------------------
+    // Assemble Features
+    // -------------------------------
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("Pclass","Age","Fare","FamilySize","SexNumeric"))
+      .setOutputCol("features")
+
+    val trainFinal = assembler.transform(trainFeatures)
+
+    // -------------------------------
+    // Train Model
+    // -------------------------------
+
+    val lr = new LogisticRegression()
+      .setLabelCol("Survived")
+      .setFeaturesCol("features")
+
+    val model = lr.fit(trainFinal)
+
+    val trainPredictions = model.transform(trainFinal)
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("Survived")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+
+    val accuracy = evaluator.evaluate(trainPredictions)
+
+    println("Training Accuracy = " + accuracy)
+
+    println("Model Training Completed")
+
+    // -------------------------------
+    // Load Test Dataset
+    // -------------------------------
+
+    val test = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("test.csv")
+
+    // Handle missing values in test dataset
+
+    val testClean = test.na.fill(Map(
+      "Age" -> avgAge,
+      "Fare" -> avgFare
+    ))
+
+    // Feature engineering on test data
+
+    val testFeatures = testClean
+      .withColumn("SexNumeric", when(col("Sex") === "male", 1).otherwise(0))
+      .withColumn("FamilySize", col("SibSp") + col("Parch"))
+
+    // Assemble features
+
+    val testFinal = assembler.transform(testFeatures)
+
+    // -------------------------------
+    // Prediction
+    // -------------------------------
+
+    val predictions = model.transform(testFinal)
+
+    predictions.select(
+      "PassengerId",
+      "prediction",
+      "probability"
+    ).show()
 
     spark.stop()
   }
